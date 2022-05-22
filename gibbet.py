000110e8c6bcb7007527957470ffd73bf78ba0b7
Kevin Picoli Silveira <47861834+Kevin6476@users.noreply.github.com>
2022-05-22 05:46:56 -0300

    Iniciando código do jogo da forca, nome cravado e sem definição para vitória do jogador :)

--- gibbet.py
+++ gibbet.py
@@ -2,8 +2,22 @@ def to_play():
     print("#############################")
     print("Welcome to the hangman game!")
     print("#############################")
-    print("End of the game!")
 
+    got_it_right = False
+    gibbet = False
+    secret_word = "banana"
+    letters_unraveled = ["_", "_", "_", "_", "_", "_"]
+
+    while not got_it_right and not gibbet:
+        print(letters_unraveled)
+        shot = input("Enter a letter: ").strip()
+        index = 0
+        for letter in secret_word:
+            if letter.lower() == shot.lower():
+                letters_unraveled[index] = letter
+            index += 1
+
+    print("End of the game!")
 
 if __name__ == "__main__":
     to_play()
