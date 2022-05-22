d724bc7fe7933ca46253bd2f2f79bbc12e6e83cf
Kevin Picoli Silveira <47861834+Kevin6476@users.noreply.github.com>
2022-05-22 00:33:46 -0300

    Criando jogo de adivinhação, testando valor e colocando em variáveis booleanas e dando dica para o usuário

--- divination.py
+++ divination.py
@@ -1,1 +1,21 @@
-print("Bem vindo no jogo de adivinhação!")
+print("#############################")
+print("Welcome to the Guessing game!")
+print("#############################")
+
+secret_num = 42
+
+shot = int(input("Enter your number: "))
+print("You typed ", shot)
+
+hit_kick = shot == secret_num
+kick_was_bigger = shot > secret_num
+kick_was_smaller = shot < secret_num
+
+if hit_kick:
+    print("You're right!")
+elif kick_was_bigger:
+    print("You missed! Your kick was bigger than the secret number")
+elif kick_was_smaller:
+    print("You missed! Your guess was less than the secret number")
+
+print("End of the game!")
