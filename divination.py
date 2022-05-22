8d2897048127bd3c829dcba4a28188e4e3c19c5c
Kevin Picoli Silveira <47861834+Kevin6476@users.noreply.github.com>
2022-05-22 01:16:51 -0300

    Adicionando recurso para mais de uma tentativa, simplificando condição de adivinhação

--- divination.py
+++ divination.py
@@ -3,19 +3,23 @@ print("Welcome to the Guessing game!")
 print("#############################")
 
 secret_num = 42
+total_attempts = 3
 
-shot = int(input("Enter your number: "))
-print("You typed ", shot)
+for attempt in range(1, total_attempts + 1):
+    print(f"\n\nAttempt {attempt} of {total_attempts}")
 
-hit_kick = shot == secret_num
-kick_was_bigger = shot > secret_num
-kick_was_smaller = shot < secret_num
+    shot = int(input("Enter your number: "))
+    print("Your kick was", shot)
 
-if hit_kick:
-    print("You're right!")
-elif kick_was_bigger:
-    print("You missed! Your kick was bigger than the secret number")
-elif kick_was_smaller:
-    print("You missed! Your guess was less than the secret number")
+    kick_was_bigger = shot > secret_num
+    kick_was_smaller = shot < secret_num
+
+    if kick_was_bigger:
+        print("You missed! Your kick was bigger than the secret number")
+    elif kick_was_smaller:
+        print("You missed! Your guess was less than the secret number")
+    else:
+        print("You're right!")
+        break
 
 print("End of the game!")
